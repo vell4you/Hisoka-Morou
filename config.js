ffcb61de76b7302a59ad0bdcdfec2c550e58494d
vell4you <107830771+vell4you@users.noreply.github.com>
2022-07-28 19:00:07 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 /**
    * Create By Dika Ardnt.
-   * Contact Me on wa.me/6288292024190
+   * Contact Me on wa.me/6286756248377
    * Follow https://github.com/DikaArdnt
 */
 
@@ -18,9 +18,9 @@ global.APIKeys = {
 }
 
 // Other
-global.owner = ['6283167714830','6281382420312','6287721317870','6288292024190']
-global.premium = ['6288292024190']
-global.packname = 'Hisoka Morrow'
+global.owner = ['6286756248377','6286756248377','6287721317870','6288292024190']
+global.premium = ['6286756248377']
+global.packname = 'Hinata BotXz'
 global.author = 'WhatsApp Bot'
 global.sessionName = 'hisoka'
 global.prefa = ['','!','.','🐦','🐤','🗿']
